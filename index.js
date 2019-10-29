67697fcb01ede27ab06fc0bd611d4d29986a8abb
Brett Slaski <brett.slaski@morningstar.com>
2019-10-29 13:43:06 -0500

    append to secret

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const run = () => {
     const bcs = process.env.BRETT_SECRET
     console.log('bcs', bcs)
     console.log('b64',Buffer.from(bcs).toString('base64'))
+    console.log('bcs', "This is before" + bcs + "This is after")
 }
 
 module.exports = {
