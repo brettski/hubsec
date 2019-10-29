3f8bfec4565f78772f67b987c1ec07f3dada6ca3
Brett Slaski <brett.slaski@morningstar.com>
2019-10-29 14:05:46 -0500

    try again

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const run = () => {
     const bcs = process.env.BRETT_SECRET
     console.log('bcs', bcs)
     console.log('b64',Buffer.from(bcs).toString('base64'))
-    console.log('bcs', bcs + 22 - 2)
+    const sum = (parseInt(bcs) + 22 - 2)
+    console.log('sum', sum)
 }
 
 module.exports = {
