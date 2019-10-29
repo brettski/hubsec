5aea8e603f5e08b7112d26e9737318cf74ff9c44
Brett Slaski <brett.slaski@morningstar.com>
2019-10-29 13:36:44 -0500

    base64 it

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config()
 const run = () => {
     const bcs = process.env.BRETT_SECRET
     console.log('bcs', bcs)
+    console.log('b64',Buffer.from(bcs).toString('base64'))
 }
 
 module.exports = {
